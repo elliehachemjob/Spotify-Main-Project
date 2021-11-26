2e1a0bdf4774fe002c20e6dd1c29e5ae2cfa5f80
elliehachemjob <elliehachemjob@gmail.com>
2021-11-26 15:20:47 +0200

    2nd component done

--- src/features/userData/Dashboard.tsx
+++ src/features/userData/Dashboard.tsx
@@ -1,4 +1,15 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import Rating from "@mui/material/Rating";
+import "../../App.css";
+import SearchComponent from "./SearchComponent";
 
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { IdSaver, TokenSaver, tokenSelector, idSelector } from "./userData";
@@ -9,25 +20,94 @@ export function Dashboard() {
   const id = useAppSelector(idSelector);
   const token = useAppSelector(tokenSelector);
 
-  console.log(` the id is ${id}`);
-  console.log(` the token is ${token}`);
+  const [searchQuery, setSearchQuery] = useState<string>("a");
+  const [value, setValue] = useState<any>();
+  const [items, setItems] = useState<any>();
+
+  const searchArtist = (e: any) => {
+    setSearchQuery(e.target.value);
+
+    axios
+      .get(
+        `https://api.spotify.com/v1/search?q=${searchQuery}&type=track%2Cartist`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      )
+      .then((response) => {
+        setItems(response.data);
+        console.log(items);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
-      <div>
-        Dashboard
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(IdSaver(1))}
-        >
-          -
-        </button>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(TokenSaver(1))}
-        >
-          +
-        </button>
+      <SearchComponent searchQuery={searchQuery} onChange={searchArtist} />
+      <div className="container">
+        {items
+          ? items.artists?.items
+              ?.filter((item: any) =>
+                item.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+              )
+
+              .map((filteredItem: any) => {
+                return (
+                  <Card className="item" sx={{ maxWidth: 300 }}>
+                    <CardContent>
+                      {filteredItem.images
+                        .filter((img: any) => img.height === 160)
+                        .map((img: any) => {
+                          return (
+                            <img
+                              src={
+                                !img.url
+                                  ? "https://www.legal.ca/public/uploads/images/noimage.jpg"
+                                  : img.url
+                              }
+                            />
+                          );
+                        })}
+
+                      <Typography>{filteredItem.name}</Typography>
+                      <Typography
+                        sx={{ fontSize: 14 }}
+                        color="text.secondary"
+                        gutterBottom
+                      >
+                        {filteredItem.followers.total} followers
+                      </Typography>
+                      <Typography
+                        sx={{ fontSize: 14 }}
+                        color="text.secondary"
+                        gutterBottom
+                      >
+                        {filteredItem.popularity} popularity
+                      </Typography>
+
+                      <Typography
+                        sx={{ position: "relative", top: 30 }}
+                        color="text.secondary"
+                      >
+                        {filteredItem.y} tracks
+                      </Typography>
+                      <Rating
+                        style={{ position: "relative", top: 25, right: 4.5 }}
+                        name="simple-controlled"
+                        value={filteredItem.popularity > 40 ? 5 : 3}
+                        // onChange={(event, newValue) => {
+                        //   setValue(5);
+                        // }}
+                      />
+                    </CardContent>
+                  </Card>
+                );
+              })
+          : null}
       </div>
     </div>
   );
--- src/features/userData/Login.tsx
+++ src/features/userData/Login.tsx
@@ -1,34 +1,113 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { IdSaver, TokenSaver, tokenSelector, idSelector } from "./userData";
+import { LocalConvenienceStoreOutlined } from "@material-ui/icons";
 
 export function Login() {
   const dispatch = useAppDispatch();
-
   const id = useAppSelector(idSelector);
   const token = useAppSelector(tokenSelector);
+  const [tokenToSave, setTokenToSave] = useState<any>();
 
-  console.log(` the id is ${id}`);
-  console.log(` the token is ${token}`);
+  const SPOTIFY_ENDPOINT_AUTHORIZATION =
+    "https://accounts.spotify.com/authorize";
+
+  const CLIENT_ID: any = "eae2c519c3084b16a48056ce021a5ae2";
+  const REDIRECT_URI: any = "http://localhost:3000/dashboard/";
+  const SCOPES: any = [
+    "user-read-playback-state",
+    "user-read-currently-playing",
+    "playlist-read-private",
+  ];
+  const DELIMITER: any = "%20";
+  const SCOPE_URI_PARAM: any = SCOPES.join(DELIMITER);
+
+  const getReturnParamsFromSpotifyAuth = (hash: any) => {
+    const stringAfterHash = hash.substring(1);
+    const paramsInUrl = stringAfterHash.split("&");
+    const paramsSplitUp = paramsInUrl.reduce(
+      (accumulator: any, currentValue: any) => {
+        console.log(currentValue);
+        const [key, value] = currentValue.split("=");
+        accumulator[key] = value;
+        return accumulator;
+      },
+      {}
+    );
+    return paramsSplitUp;
+  };
+
+  useEffect(() => {
+    if (window.location.hash) {
+      const { access_token, expires_in, token_type } =
+        getReturnParamsFromSpotifyAuth(window.location.hash);
+      dispatch(TokenSaver({ access_token }));
+      console.log({ access_token });
+      console.log(token);
+      localStorage.clear();
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("expiresIn", expires_in);
+      localStorage.setItem("tokenType", token_type);
+    }
+  });
+
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      setTokenToSave(localStorage.getItem("accessToken"));
+      dispatch(TokenSaver(tokenToSave));
+    }
+  }, []);
+
+  const handleLogin = () => {
+    // @ts-ignore
+    window.location = `${SPOTIFY_ENDPOINT_AUTHORIZATION}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE_URI_PARAM}&response_type=token&show_dialog=true`;
+  };
+  const useStyles = makeStyles({
+    login: {
+      display: "grid",
+      placeItems: "center",
+      height: "100vh",
+      backgroundColor: "black",
+
+      "& img": {
+        width: "50%",
+      },
+
+      "& a": {
+        padding: "20px",
+        borderRadius: "99px",
+        backgroundColor: "#1db954",
+        fontWeight: 600,
+        color: "white",
+        textDecoration: "none",
+      },
+
+      "& a:hover": {
+        backgroundColor: " white",
+        borderColor: "#1db954",
+        color: "#1db954",
+      },
+    },
+  });
+  const classes = useStyles();
+
+  //   <button
+  //   aria-label="Decrement value"
+  //   onClick={() => dispatch(IdSaver(1))}
+  // ></button>
+  // <button
+  //   aria-label="Increment value"
+  //   onClick={() => dispatch(TokenSaver(1))}
+  // ></button>
 
   return (
-    <div>
-      Login
-      <div>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(IdSaver(1))}
-        >
-          -
-        </button>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(TokenSaver(1))}
-        >
-          +
-        </button>
-      </div>
+    <div className={classes.login}>
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="Spotify-Logo"
+      />
+      <a onClick={handleLogin}>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
